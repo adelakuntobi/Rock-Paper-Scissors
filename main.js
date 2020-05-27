8de64c5aa9742cbcc453814281de9d5e7f76cda4
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2020-05-27 22:16:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,66 @@
+/**
+* LOGIC
+*/
+var modal = document.getElementById("myModal");
+var openButton = document.getElementById("openButton");
+var closeBtn = document.getElementById("closeBtn");
+
+var paper = document.getElementById('paper'),
+ scissors = document.getElementById('scissors'),
+ rock = document.getElementById('rock'),
+
+ win = document.getElementById('win'),
+ choose = document.getElementById('choose'); 
+
+
 var choices = ["paper", "rock", "scissors"];
-var i = Math.floor(Math.random() * 3);
-var ComChoice = choices[i];
 var UserPoints = 0;
-var ComPoints = 0;
-function score(){
-	var score_div = document.getElementById("score").innerHTML = UserPoints + " - " + ComPoints;
+
+
+
+rock.addEventListener("click",function () {
+  check("rock");
+})
+paper.addEventListener("click",function () {
+  check("paper");
+})
+scissors.addEventListener("click",function () {
+  check("scissors");
+})
+
+var id
+function check(id){
+  var i = Math.floor(Math.random() * 3);
+  var ComChoice = choices[i];
+  choose.textContent =   'The computer choose: ' + choices[i];
+  if (ComChoice === id){
+    win.textContent = "Its a draw!"
+    console.log('draw')
+  }
+  else if(id === "paper" && ComChoice === "rock" || id === "rock" && ComChoice === "scissors" || id === "scissors" && ComChoice === "paper"){
+    document.getElementById("score").textContent = UserPoints++;
+    win.textContent;win.textContent = "Genius, You Won"
+    console.log('You won!')
+  }
+  else{
+    win.textContent = "You could try again!"
+    console.log('Loser')
+  }
 }
-setInterval(score, 50);
-function convert(word){
-	if(word === "paper") return '<i class="far fa-hand-paper"></i>';
-	if(word === "rock") return '<i class="far fa-hand-rock"></i>';
-	return '<i class="far fa-hand-scissors"></i>'
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
-function game(UserChoice){
-	var box = document.getElementById("challenge");
-	box.style.display = "inline-flex";
-	var userDiv = document.getElementById("YourObject");
-	userDiv.innerHTML = convert(UserChoice);
-	var comDiv = document.getElementById("ComObject");
-	comDiv.innerHTML = convert(ComChoice);
-	if(UserChoice === "paper" && ComChoice === "rock" || UserChoice === "rock" && ComChoice === "scissors" || UserChoice === "scissors" && ComChoice === "paper"){
-		win(UserChoice);
-	}
-	else if(UserChoice === ComChoice){
-		draw(UserChoice);
-	}
-	else{
-		lose(UserChoice);
-	}
-	function continuGame(){
-		i = Math.floor(Math.random() * 3);
-		ComChoice = choices[i];
-		box.style.display = "none";
-	}
-	setTimeout(continuGame, 1200);
+
+closeBtn.addEventListener('click',displayModal);
+openButton.onclick = function() {
+  modal.style.display = "block";
 }
-function win(bn){
-	UserPoints++;
-    document.getElementById("who").innerHTML = "You win!";
-    var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    bn.classList.add("green");
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        bn.classList.remove("green");
-    }, 1200);
-}
-function draw(bn){
-	document.getElementById("who").innerHTML = "It's a Draw.";
-	var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    bn.classList.add("gray");
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        bn.classList.remove("gray");
-    }, 1200);
-}
-function lose(bn){
-	ComPoints++;
-	document.getElementById("who").innerHTML = "You lose...";
-	var bn = document.getElementById(bn);
-    bn.classList.remove("bn");
-    bn.classList.add("red");
-    setTimeout(() => {
-    	bn.classList.add("bn");
-        bn.classList.remove("red");
-    }, 1200);
+
+function displayModal() {
+  modal.style.display = "none";
 }
